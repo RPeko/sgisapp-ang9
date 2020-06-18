af31fe0eaba16434501e6598c40d23686c0e4ca2
Peko Roganovic <peko.roganovic@gmail.com>
2020-06-18 11:14:07 +0200

    server

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/gisapp'));
+app.use(express.static(__dirname + '/dist/sgisapp'));
 app.set('port', process.env.PORT || 5000);
 
 app.get('/*', function(req,res) {  
 
-res.sendFile(path.join(__dirname+'/dist/gisapp/index.html'));
+res.sendFile(path.join(__dirname+'/dist/sgisapp/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
