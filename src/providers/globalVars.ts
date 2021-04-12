8f27775bd75e5f1999ff6c9ebdd262965ef4da56
rpeko <peko.roganovic@gmail.com>
2021-04-12 12:59:52 +0200

    version

--- src/providers/globalVars.ts
+++ src/providers/globalVars.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class GlobalVars {
   // baseURL = 'http://localhost:8080';
-   baseURL = 'http://79.101.21.143:8070/sgis-1.0.2';
+   baseURL = 'http://79.101.21.143:8070/sgis-1.0.3';
 
   constructor() {
   }
