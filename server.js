8ea0f736cef023d5b5806e97d9658af191b0203d
Peko Roganovic <peko.roganovic@gmail.com>
2021-09-06 08:06:38 +0200

    redirect to ssl

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ app.enable('trust proxy');
 app.use(express.static(__dirname + '/dist/sgisapp'));
 app.set('port', process.env.PORT || 5000);
 app.get('/*', function(req,res) {  
-
 res.sendFile(path.join(__dirname+'/dist/sgisapp/index.html'));
 });
+
+// enable ssl redirect
 app.use(sslRedirect());
+
 // Start the app by listening on the default Heroku port
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
