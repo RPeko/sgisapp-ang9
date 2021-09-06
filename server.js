683f3b5a938414017bbdcb98605933b7fb6c1c6c
Peko Roganovic <peko.roganovic@gmail.com>
2021-09-06 07:55:34 +0200

    redirect to ssl

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 //Install express server
+var sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
 const path = require('path');
 
 const app = express();
+app.enable('trust proxy');
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/sgisapp'));
@@ -11,14 +13,7 @@ app.get('/*', function(req,res) {
 
 res.sendFile(path.join(__dirname+'/dist/sgisapp/index.html'));
 });
-app.enable('trust proxy');
-app.use(function (req, res, next) {
-  if (req.header('x-forwarded-proto') === 'http') {
-    res.redirect(301, 'https://' + req.hostname + req.url);
-    return
-  }
-  next()
-});
+app.use(sslRedirect());
 // Start the app by listening on the default Heroku port
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
