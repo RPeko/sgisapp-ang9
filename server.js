a645314ccc6f361146402eb46347dedc503f3a50
Peko Roganovic <peko.roganovic@gmail.com>
2021-09-06 07:33:05 +0200

    redirect to https

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/sgisapp'));
 app.set('port', process.env.PORT || 5000);
-app.use(function (req, res, next) {
-    if (req.header('x-forwarded-proto') === 'http') {
-      res.redirect(301, 'https://' + req.hostname + req.url);
-      return
-    }
-    next()
-  });
-
 app.get('/*', function(req,res) {  
 
 res.sendFile(path.join(__dirname+'/dist/sgisapp/index.html'));
+});
+app.enable('trust proxy');
+app.use(function (req, res, next) {
+  if (req.header('x-forwarded-proto') === 'http') {
+    res.redirect(301, 'https://' + req.hostname + req.url);
+    return
+  }
+  next()
 });
 
 // Start the app by listening on the default Heroku port
